6fb9a9820aa8dd71f48760f303f2d053c8beef04
ebifrier <ebifrier@gmail.com>
2015-05-14 23:19:43 +0900

    テスト用のメソッドを修正しました。

--- Test/Program.cs
+++ Test/Program.cs
@@ -9,15 +9,17 @@ namespace Test
         [STAThread]
         static void Main(string[] args)
         {
-            Voiceroid voiceroid = VoiceroidFactory.CreateAkane();
+            var voiceroid = VoiceroidFactory.CreateAi();
+            var voiceroid2 = VoiceroidFactory.CreateShouta();
 
             string text = "えびちゃんわーい";
+            voiceroid2.PrepareTalk();
             if (voiceroid.PrepareTalk())
             {
-                for (int i = 0; i < 1; ++i)
+                for (int i = 0; i < 10; ++i)
                 {
                     //plugin.Talk("abcdﾊﾝｶｸ！！");
-                    voiceroid.Talk(text);
+                    voiceroid.Talk(text + i);
                     //plugin.Talk("湖湖湖湖湖湖湖湖");
 
                     System.Threading.Thread.Sleep(2400);
